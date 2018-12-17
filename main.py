38eae4770eb1155d13c7a0e3f96e9e3a6d59259e
tingerbin <xbin37@gmail.com>
2018-12-18 01:01:16 +0800

    主函数，每个模块不是被导入的，解释器就会解释为顶级，顶级模块的__name__=__main__。

---
+++ main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+import python_study_packet1
+
+print('全局变量为：',python_study_packet1.pinetree)
---
+++ python_study_packet1.py
@@ -0,0 +1,15 @@
+#!usr/bin/env pyhont3
+
+pinetree = '我是一棵松树。\n'
+
+def fun_chrismastree():
+    pinetree = '我身上挂满了礼物，我是一颗圣诞树。@^_^@ ……\n'
+    print(pinetree)
+
+if __name__ == '__main__':
+    print('\n 下雪了 \n')
+    print('===================开始做梦====================\n')
+    fun_chrismastree()
+    print('===================梦醒来了====================\n')
+    print(pinetree)
+
